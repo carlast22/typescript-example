2d18a9c1711acc65f3d4fb97d9caa9d5b449b77a
Carla Suarez <carlast22@gmail.com>
2020-04-09 22:01:46 -0500

    Install prettier and format code

--- components/App.tsx
+++ components/App.tsx
@@ -5,41 +5,48 @@ export const Item = styled.div`
   display: flex;
   justify-content: center;
   padding: 5px;
-`;
+`
 
 export const Grid = styled.div`
   display: grid;
   grid-template-rows: 50px 50px;
   grid-gap: 5px;
-`;
+`
 
 interface Props {
-    initialNumber: number
+  initialNumber: number
 }
 
-const App: React.FunctionComponent<Props> = ({initialNumber}) => {    
-    const [number, setNumber] = React.useState(initialNumber)
+const App: React.FunctionComponent<Props> = ({ initialNumber }) => {
+  const [number, setNumber] = React.useState(initialNumber)
 
-    function handleAddition() {
-        setNumber(number + 1)
-    }
+  function handleAddition() {
+    setNumber(number + 1)
+  }
 
-    function handleSubtraction() {
-        setNumber(number - 1)
-    }
+  function handleSubtraction() {
+    setNumber(number - 1)
+  }
 
-        return (
-        <>
-        <Grid>
-            <Item></Item>
-            <Item></Item>
-            <Item><button onClick={handleAddition}>+</button></Item>
-            <Item><div>{number}</div></Item>
-            <Item><button disabled={number === 0} onClick={handleSubtraction}>-</button></Item>
-             
-        </Grid>
+  return (
+    <>
+      <Grid>
+        <Item></Item>
+        <Item></Item>
+        <Item>
+          <button onClick={handleAddition}>+</button>
+        </Item>
+        <Item>
+          <div>{number}</div>
+        </Item>
+        <Item>
+          <button disabled={number === 0} onClick={handleSubtraction}>
+            -
+          </button>
+        </Item>
+      </Grid>
     </>
-        )
+  )
 }
 
 export default App
--- index.tsx
+++ index.tsx
@@ -3,5 +3,3 @@ import { render } from 'react-dom'
 import App from './components/App'
 
 render(<App initialNumber={0} />, document.getElementById('root'))
-
-
