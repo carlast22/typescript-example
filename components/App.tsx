ede04d616b4e4787e330a544ec835d5acea83fc0
Carla Suarez <carlast22@gmail.com>
2020-04-09 20:46:16 -0500

    Add counter example with ParcelJS, Typescript, React and Styled Component

---
+++ components/App.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Item = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 5px;
+`;
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-rows: 50px 50px;
+  grid-gap: 5px;
+`;
+
+const App: React.FunctionComponent = () => {    
+    const [number, setNumber] = React.useState(0)
+
+    function handleAddition() {
+        setNumber(number + 1)
+    }
+
+    function handleSubtraction() {
+        setNumber(number - 1)
+    }
+
+        return (
+        <>
+        <Grid>
+            <Item></Item>
+            <Item></Item>
+            <Item><button onClick={handleAddition}>+</button></Item>
+            <Item><div>{number}</div></Item>
+            <Item><button disabled={number === 0} onClick={handleSubtraction}>-</button></Item>
+             
+        </Grid>
+    </>
+        )
+}
+
+export default App
