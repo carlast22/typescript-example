cb8c627c6f6baba6e168732f223ef4c39e6224ef
Carla Suarez <carlast22@gmail.com>
2020-04-09 22:24:03 -0500

    Add ci steps

--- index.tsx
+++ index.tsx
@@ -2,4 +2,4 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './components/App'
 
-render(<App initialNumber={0} />, document.getElementById('root'))
+render(<App initialNumber={'0'} />, document.getElementById('root'))
